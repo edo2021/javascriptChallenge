51412e2c3f6b21c6444f37b486fdf456e33d0587
edris gemtessa <edogemtessa@gmail.com>
2021-04-18 15:43:08 -0500

    selecting the form

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,9 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// assigning the data to a variable and selecting the button
+var ufoData = data;
+
+
+var button = d3.select("#filter-btn");
+
 // assign the data from data.js to a variable
 var ufoData = data;
 
 // select the button
 var button = d3.select("#filter-btn");
+
+// select the form
+var form = d3.select("#form");
+
 
