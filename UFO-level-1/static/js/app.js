4071787e4c2a0f2bed58a5bd1a39c0230f046320
edris gemtessa <edogemtessa@gmail.com>
2021-04-18 15:45:22 -0500

    event handler created

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -18,3 +18,13 @@ var button = d3.select("#filter-btn");
 var form = d3.select("#form");
 
 
+
+// create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// complete the event handler function for form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
